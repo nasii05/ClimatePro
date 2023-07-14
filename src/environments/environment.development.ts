f45fd00c7758406eacee223c5f54bfaa218b0c06
Nasih <iamnasih@gmail.com>
2023-07-14 16:24:36 +0530

    removed api keys

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  weatherUrl:"http://api.openweathermap.org/data/2.5/forecast?id=524901",
-  weatherkey:"8b0fccc5c44b39eefa3e6cebb10b1dca",
-  geo:"https://wft-geo-db.p.rapidapi.com/v1/geo/cities?",
-  geokey:"3ffa2a6230msh7bf17bc6e74ca5dp1ab238jsn5f8733a31a8c"
-
-};
